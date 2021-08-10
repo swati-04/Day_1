72a6dc9fe40f64289b6e0f97923d69c4fc176242
swati-04 <itwa0407@gmail.com>
2021-08-10 10:36:15 +0530

    first component and render

--- src/firstComponent.js
+++ src/firstComponent.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { render } from "react-dom";
-function FirstComponent{}{
+import {render} from "react-dom";
+function FirstComponent(){
     return(
         <React.Fragment>
-        <div>
-        </div>  
+    
             <div>
                 <h2>This is my first component</h2>
+                <img src="shinchan-wallpapers-01.jpg" alt="my favorite cartoon"
+                width="500" height="320"></img>
             </div>
         </React.Fragment>
          )
