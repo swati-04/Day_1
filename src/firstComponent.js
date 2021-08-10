f90bba10def261f22c86ffb12e2405f975404891
swati-04 <itwa0407@gmail.com>
2021-08-10 11:18:22 +0530

    components in react

--- src/firstComponent.js
+++ src/firstComponent.js
@@ -1,15 +1,15 @@
 import React from "react";
-import {render} from "react-dom";
-function FirstComponent(){
-    return(
+import { render } from "react-dom";
+function FirstComponent() {
+    return (
         <React.Fragment>
-    
+
             <div>
                 <h2>This is my first component</h2>
                 <img src="shinchan-wallpapers-01.jpg" alt="my favorite cartoon"
-                width="500" height="320"></img>
+                    width="400" height="320"></img>
             </div>
         </React.Fragment>
-         )
+    )
 }
 export default FirstComponent
---
+++ src/mycomponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from "react-dom";
+function MyComponent() {
+    return (
+        <React.Fragment>
+
+            <div>
+                <h1>Swati Singh</h1>
+                <h2>22</h2>
+                <img src="swati.jpeg" alt="my favorite cartoon"
+                    width="250" height="320"></img>
+            </div>
+        </React.Fragment>
+    )
+}
+export default MyComponent
