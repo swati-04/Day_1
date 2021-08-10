adb920c870bb2bccd77029c57bd89d6b1ea1adff
swati-04 <itwa0407@gmail.com>
2021-08-10 14:55:19 +0530

    css styling and react components

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -17,7 +17,7 @@ const Book=()=>{
 }
 const Img=()=><img src="https://m.media-amazon.com/images/I/71gpkRk86eS._AC_UY218_.jpg"
 alt=""></img>
-const Title=()=> <h1>Think and grow rich</h1>
-const Author=()=><h2>Napolean Hill</h2>
+const Title=()=> <h1 style={{color:"black",font:"bold",fontSize:"1.5rem",marginTop:"0.25"}}>Think and grow rich</h1>
+const Author=()=><h2 style={{color:'#617d98',fontSize:'0.75rem',marginTop:"0.25rem",letterSpacing:"2px"}} >Napolean Hill</h2>
 
 export default Bookstore
